10b4d73dc99d0638f91a5c90127fca52b76ed7cf
raflasr <162029447+raflasr@users.noreply.github.com>
2024-10-10 19:05:26 +0700

    Create storabel.java

---
+++ TugasAsistensi TemaApotek/storabel.java
@@ -0,0 +1,5 @@
+interface Storable {
+    void tambahItem(String item, int jumlah); // Menambah item ke dalam inventory
+    void hapusItem(String item); // Menghapus item dari inventory
+    void tampilkanItem(); // Menampilkan semua item dalam inventory
+}
