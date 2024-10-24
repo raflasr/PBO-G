f7801b6d3a1d5837ab75e693245e3f023f9391ce
raflasr <162029447+raflasr@users.noreply.github.com>
2024-10-24 14:23:04 +0700

    Create main.java

---
+++ ETS/No.2 Book/main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Membuat objek Book menggunakan konstruktor
+        Book buku1 = new Book("Sang Pemimpi", "Andrea Hirata", 2006, "Bentang Pustaka", "Novel");
+
+        // Memanggil metode untuk mencetak detail buku
+        buku1.printDetails();
+    }
+}
