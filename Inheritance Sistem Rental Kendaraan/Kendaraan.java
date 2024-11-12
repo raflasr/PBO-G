e2244d7bfc557dd3d515a4912e7e39380d9b7d00
raflasr <162029447+raflasr@users.noreply.github.com>
2024-11-12 15:40:10 +0700

    Create Kendaraan.java

---
+++ Inheritance Sistem Rental Kendaraan/Kendaraan.java
@@ -0,0 +1,15 @@
+public class Kendaraan {
+    protected String merk;
+    protected String model;
+    protected int tahunProduksi;
+
+    public Kendaraan(String merk, String model, int tahunProduksi) {
+        this.merk = merk;
+        this.model = model;
+        this.tahunProduksi = tahunProduksi;
+    }
+
+    public String info() {
+        return "Merk: " + merk + ", Model: " + model + ", Tahun Produksi: " + tahunProduksi;
+    }
+}
