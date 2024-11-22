689c293788fd7370c707b8a426ee456d11da5dc8
raflasr <162029447+raflasr@users.noreply.github.com>
2024-11-22 12:15:10 +0700

    Create Manusia.java

---
+++ Implementasi Abstraksi Kelas/No. 1/Manusia.java
@@ -0,0 +1,12 @@
+// Kelas Manusia
+public class Manusia extends MakhlukHidup {
+    @Override
+    public void bernafas() {
+        System.out.println("Manusia bernafas menggunakan paru-paru.");
+    }
+
+    @Override
+    public void makan() {
+        System.out.println("Manusia makan makanan yang dimasak.");
+    }
+}
